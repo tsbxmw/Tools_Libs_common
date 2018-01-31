dff4ee81c561dfe4b9febb933113cec9fdb225d9
wei.meng <1050636648@qq.com>
2018-01-31 11:17:17 +0800

    add new all2string(T1,T2)

--- all2string_lib_cpp/all2string.h
+++ all2string_lib_cpp/all2string.h
@@ -12,12 +12,18 @@ public:
     string Int2String(int value);
     string Float2String(float value);
     string Double2String(double value);
-    template <class T>
+
+    template <class T> inline
     string all2string(T t);
+
+    template <class T1, class T2>
+    string all2string(T1 t1, T2 t2);
+    
+
 };
 
 
-template <class T>
+template <class T> inline
 string All2String::all2string(T t)
 {
     stringstream ss;
@@ -25,4 +31,10 @@ string All2String::all2string(T t)
     ss << t;
     ss >> str;
     return str;
+}
+
+template <class T1, class T2> 
+string All2String::all2string(T1 t1, T2 t2)
+{
+    return all2string(t1) + " " + all2string(t2 );
 }
