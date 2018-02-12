51192116ee4ecb9c4ab598b959da626849d113a7
tsbxmw <1050636648@qq.com>
2018-02-12 15:36:06 +0800

    add float test to test.cpp

--- all2string_lib_cpp/test.cpp
+++ all2string_lib_cpp/test.cpp
@@ -9,5 +9,7 @@ int main()
     All2String a2s;
     cout << a2s.all2string(5) << endl;
     cout << a2s.all2string(5,10) << endl;
+    cout << a2s.all2string(100.00) << endl;
+    cout << a2s.all2string(000.00) << endl;
     return 0;
 }
