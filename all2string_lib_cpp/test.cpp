c43bfd403671113671a423caede1004bc6cf049f
wei.meng <1050636648@qq.com>
2018-02-28 14:44:29 +0800

    delete other test.... for now

--- all2string_lib_cpp/test.cpp
+++ all2string_lib_cpp/test.cpp
@@ -11,8 +11,5 @@ int main()
     cout << a2s.all2string(5,10) << endl;
     cout << a2s.all2string(100.00) << endl;
     cout << a2s.all2string(000.00) << endl;
-    cout << All2String::Int2String(222) << endl;
-    cout << All2String::Float2String(100.00) << endl;
-    cout << All2String::Double2String(100.00000) << endl;
     return 0;
 }
